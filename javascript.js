e4ca0a3628b3372e0c543a3438619baa2f801d06
A.H <akkhrtaus@yahoo.com>
2026-09-13 15:31:40 +0900

    Add the logic to change hovered grid's background colour

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
 const container = document.getElementById("container");
 
+function changeBGColour(obj){
+    obj.setAttribute("class", "grid-hovered");
+}
+
 for (let r = 1; r <= 16; r++) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
@@ -8,9 +12,11 @@ for (let r = 1; r <= 16; r++) {
     for (let c = 1; c <= 16; c++) {
         const grid = document.createElement("div");
         grid.setAttribute("class", "grid");
+        grid.setAttribute("onmouseover", "changeBGColour(this)");
         // grid.textContent = c;
         row.appendChild(grid);
     }
 
     container.appendChild(row);
 }
+
