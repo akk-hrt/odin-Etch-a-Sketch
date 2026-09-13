68053e2ee63398230a0605ba758094fe4f7303e4
akk-hrt <akkhrtaus@yahoo.com>
2026-09-13 15:56:16 +0900

    Completed the Project: Etch-a-Sketch as per instructions

--- javascript.js
+++ javascript.js
@@ -4,12 +4,24 @@ function changeBGColour(obj){
     obj.setAttribute("class", "grid-hovered");
 }
 
-for (let r = 1; r <= 16; r++) {
+function createNewCanvas(){
+    container.textContent = "";
+    let num = prompt("Please enter the number (16-100) of squares per side for the new grid.", "30");
+
+    if (num <16) {
+        num = 16;
+    } else if (num > 100) {
+        num = 100;
+    } else if (!Number.isInteger(num)) {
+        num = Math.floor(Math.random() * 100);
+    }
+
+    for (let r = 1; r <= num; r++) {
     const row = document.createElement("div");
     row.setAttribute("class", "row");
     row.setAttribute("id", "row" + r);
     
-    for (let c = 1; c <= 16; c++) {
+    for (let c = 1; c <= num; c++) {
         const grid = document.createElement("div");
         grid.setAttribute("class", "grid");
         grid.setAttribute("onmouseover", "changeBGColour(this)");
@@ -19,4 +31,7 @@ for (let r = 1; r <= 16; r++) {
 
     container.appendChild(row);
 }
+}
+
+
 
